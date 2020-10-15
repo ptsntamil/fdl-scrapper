e83da164e109d1fe331f670009d8f6573f411d1b
Tamilarasan Pannerselvam <ptsntamil@gmail.com>
2020-10-15 19:47:51 +0530

    Fix: Company details

--- credentials.js
+++ credentials.js
@@ -1,9 +1,4 @@
-// exports.credentials = {
-//   username: "vsv.fdl@gmail.com",
-//   password: "0818vsv@",
-// };
-
 exports.credentials = {
-  username: "tharani.fdl@gmail.com",
-  password: "tharani98",
+  username: "",
+  password: "",
 };
